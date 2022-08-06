8ef2e99c1fcda8579bb2b08658c4b107d321d720
Jordi <jordi.hu@gmail.com>
2022-08-06 16:47:40 +0800

    updated projects park data

--- assets/script/game/role/view/RoleViewProjectsPark.ts
+++ assets/script/game/role/view/RoleViewProjectsPark.ts
@@ -40,15 +40,21 @@ export class RoleViewProjectsPark extends CCComp {
 
     private projectName: string[] = [
         '孵化器 C-Combinator',
-        'Web3人人造星'
+        'Web3人人造星',
+        'HYDAO',
+        'SG'
     ];
     private projectDesc: string[] = [
         'SeeDAO 是一个/基于/社区预测协议的/ DAO 的孵化器。坚持 DAO IT, DO IT, 信奉 Co-build, Co-share。C-Combinator 是 SeeDAO 推出的社区孵化平台，旨在为 Web3 创业者提供最前沿的教学指导、强大的校友网络与社区支持，成为 Web3 的创新助产师。',
-        'AM DAO是web3的艺术类去中心化组织，打造加密世界顶级文艺娱乐社区。社区聚集创作者和爱好者，构建文艺活动平台，社区内有NFT作品主题创作、作品宣传发售、元宇宙场景互动场所等文艺生态。\r\n\r\n“人人造星”是系列主题活动，这次是第一期活动。每次“人人造星”活动，都会由明星艺人、和对应的明星作品，都要给相应的title。'
+        'AM DAO是web3的艺术类去中心化组织，打造加密世界顶级文艺娱乐社区。社区聚集创作者和爱好者，构建文艺活动平台，社区内有NFT作品主题创作、作品宣传发售、元宇宙场景互动场所等文艺生态。\r\n\r\n“人人造星”是系列主题活动，这次是第一期活动。每次“人人造星”活动，都会由明星艺人、和对应的明星作品，都要给相应的title。',
+        '去中心化自制组织DAO正在成为Web3世界的新趋势，它在共同协作、自身治理等方面的好处，也许有助于我们重新思考“电台”。海盗电台（HYDAO.io） 由去中心化自治组织驱动，是一个以 Web3 的方式谈论 Web3 的播客。希望和你一同航行，乘风逐浪于加密之海！',
+        'Sg is a all-in-one DAO visualization platform. It is designed to bring people into the world of DAOs and help them adapt to their new future of work. Whether people want to find a DAO in an interesting field and start contributing, earn some extra money from it. Sg is built combined with the virtual office scene, and DAO presentation and then some on-chain records are used to solve some identity and contribution record problems.'
     ];
     private projectContact: string[] = [
         'Tx#2221',
-        'PinkPing AM Dao 加密世界顶级文艺娱乐社区#5339'
+        'PinkPing AM Dao 加密世界顶级文艺娱乐社区#5339',
+        '0x加菲众#2031',
+        'ScaSte#5829'
     ];
 
     private ccLinks: { linkName: string, linkAddress: string }[] = [
@@ -63,12 +69,22 @@ export class RoleViewProjectsPark extends CCComp {
         { linkName: '投稿参赛（已结束）', linkAddress: 'https://airtable.com/shrgfx2NL5XfFSuaE' },
         { linkName: '参赛选手绑定电子钱包链接', linkAddress: 'https://airtable.com/shrnTEo7Otb6HI2Z9' }
     ];
+    private hydaoLinks: { linkName: string, linkAddress: string }[] = [
+        { linkName: '如何用 DAO 来建立一个海盗电台？', linkAddress: 'https://www.hydao.io/episode/hydaoradio' }
+    ];
+    private sgLinks: { linkName: string, linkAddress: string }[] = [
+        { linkName: '认识SG', linkAddress: 'https://heavy-language-a56.notion.site/a5a7b18427b74f93bb663b5d8e457e02' },
+        { linkName: '对SG的想法', linkAddress: 'https://airtable.com/shrgfx2NL5XfFSuaE' },
+        { linkName: '关于SG的想法', linkAddress: 'https://airtable.com/shrnTEo7Otb6HI2Z9' }
+    ];
 
     private allLinks = [];
 
     onLoad() {
         this.allLinks.push(this.ccLinks);
         this.allLinks.push(this.amdaoLinks);
+        this.allLinks.push(this.hydaoLinks);
+        this.allLinks.push(this.sgLinks);
         this.initContent();
     }
 
